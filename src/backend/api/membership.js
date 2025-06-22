cbaf946901d6e59c43c62850672922b5744d482d
Septian Pahmi <80021312+septianpahmi@users.noreply.github.com>
2025-06-22 14:38:02 +0700

    Update membership.js

--- src/backend/api/membership.js
+++ src/backend/api/membership.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import { pool } from "../db.js";
+import { pool } from "./db.js";
 
 const app = express();
 app.use(cors());
