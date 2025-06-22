72a67d3481da207dcb904e49df0e1a93b348014e
Septian Pahmi <80021312+septianpahmi@users.noreply.github.com>
2025-06-22 14:50:21 +0700

    Update db.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -3,9 +3,9 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
-  port: 5432,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }, // untuk Supabase
+  ssl: { rejectUnauthorized: false },
 });
