06d2e21eda3812c032d11d335e68b804a5f88238
Septian Pahmi <80021312+septianpahmi@users.noreply.github.com>
2025-06-22 14:57:03 +0700

    Update db.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
